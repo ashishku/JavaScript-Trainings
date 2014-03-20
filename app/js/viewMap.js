a78e59b265cf6bba01d9eb2313e9e08fa2ad3c3f
OpenShift guest <ashish.kvarma@gmail.com>
2014-03-20 01:17:05 -0400

    till session 2-3

--- app/js/viewMap.js
+++ app/js/viewMap.js
@@ -1,35 +1,86 @@
-function genrateHtmlSessionDetails (options) {
-  function getTemplate() {
+function getTemplate(options) {
     return options.template ? options.template : (options.ssid ? 'sessions/' + options.sid + '/' + options.sid + '_' + options.ssid + '.html' : 'sessions/' + options.sid + '.html')
-  }
+}
+function genrateHtmlSessionDetails (options) {
   return {
     view     : 'Html',
     next     : options.next ? options.next : 'session' + options.sid + '_' + (parseInt(options.ssid) + 1),
     previous : options.previous ? options.previous : 'session' + options.sid + '_' + (parseInt(options.ssid) - 1),
     data     : {
-      template: getTemplate()
+      template: getTemplate(options)
+    }
+  }
+}
+
+function genrateJsBinSessionDetails (options) {
+  return {
+    view     : 'Jsbin',
+    next     : options.next ? options.next : 'session' + options.sid + '_' + (parseInt(options.ssid) + 1),
+    previous : options.previous ? options.previous : 'session' + options.sid + '_' + (parseInt(options.ssid) - 1),
+    data     : {
+      url: options.url
+    }
+  }
+}
+
+function genrateSummarySessionDetails (options) {
+  return {
+    view     : 'Summary',
+    next     : options.next ? options.next : 'session' + options.sid + '_1',
+    previous : options.previous,
+    data     : {
+      session    : options.session,
+      subSessions: options.subSessions,
     }
   }
 }
 
 define({
-    'session1'   : genrateHtmlSessionDetails({sid:1, next: 'session1_1', previous: '#', data:{template: 'sessions/1.html'}}),
-    'session1_1' : genrateHtmlSessionDetails({sid:1, ssid: '1', previous: 'session1'}),
-    'session1_2' : genrateHtmlSessionDetails({sid:1, ssid: '2'}),
-    'session1_3' : genrateHtmlSessionDetails({sid:1, ssid: '3'}),
-    'session1_4' : genrateHtmlSessionDetails({sid:1, ssid: '4'}),
-    'session1_5' : genrateHtmlSessionDetails({sid:1, ssid: '5'}),
-    'session1_6' : genrateHtmlSessionDetails({sid:1, ssid: '6'}),
-    'session1_7' : {view: 'Jsbin',  next:'session1_8', previous: 'session1_6', data:{url: '//jsbin.com/ashishJS/2/embed?js,console'}},
-    'session1_8' : genrateHtmlSessionDetails({sid:1, ssid: '8'}),
-    'session1_9' : genrateHtmlSessionDetails({sid:1, ssid: '9'}),
-    'session1_10': genrateHtmlSessionDetails({sid:1, ssid: '10'}),
-    'session1_11': genrateHtmlSessionDetails({sid:1, ssid: '11'}),
-    'session1_12': genrateHtmlSessionDetails({sid:1, ssid: '12'}),
-    'session1_13': genrateHtmlSessionDetails({sid:1, ssid: '13'}),
-    'session1_14': genrateHtmlSessionDetails({sid:1, ssid: '14'}),
-    'session1_15': genrateHtmlSessionDetails({sid:1, ssid: '15'}),
-    'session1_16': genrateHtmlSessionDetails({sid:1, ssid: '16', next: 'session2'}),
+    'session1'   : genrateSummarySessionDetails({
+        sid:1, 
+        previous: '#', 
+        session: 'Introduction',
+        subSessions: [
+            { url: '#side/session1_1', title: 'Introduction' },
+            { url: '#side/session1_2', title: 'Lexical Structure' },
+            { url: '#side/session1_4', title: 'Variables and Data Types' },
+            { url: '#side/session1_8', title: 'Expressions' },
+            { url: '#side/session1_9', title: 'Operators' },
+            { url: '#side/session1_15', title: 'Eval' }
+        ]
+    }),
+    'session1_1' : genrateHtmlSessionDetails   ({sid:1, ssid: '1', previous: 'session1'}),
+    'session1_2' : genrateHtmlSessionDetails   ({sid:1, ssid: '2'}),
+    'session1_3' : genrateHtmlSessionDetails   ({sid:1, ssid: '3'}),
+    'session1_4' : genrateHtmlSessionDetails   ({sid:1, ssid: '4'}),
+    'session1_5' : genrateHtmlSessionDetails   ({sid:1, ssid: '5'}),
+    'session1_6' : genrateHtmlSessionDetails   ({sid:1, ssid: '6'}),
+    'session1_7' : genrateJsBinSessionDetails  ({sid:1, ssid: '7', url: '//jsbin.com/ashishJS/2/embed?js,console'}),
+    'session1_8' : genrateHtmlSessionDetails   ({sid:1, ssid: '8'}),
+    'session1_9' : genrateHtmlSessionDetails   ({sid:1, ssid: '9'}),
+    'session1_10': genrateHtmlSessionDetails   ({sid:1, ssid: '10'}),
+    'session1_11': genrateHtmlSessionDetails   ({sid:1, ssid: '11'}),
+    'session1_12': genrateHtmlSessionDetails   ({sid:1, ssid: '12'}),
+    'session1_13': genrateHtmlSessionDetails   ({sid:1, ssid: '13'}),
+    'session1_14': genrateHtmlSessionDetails   ({sid:1, ssid: '14'}),
+    'session1_15': genrateHtmlSessionDetails   ({sid:1, ssid: '15'}),
+    'session1_16': genrateHtmlSessionDetails   ({sid:1, ssid: '16', next: 'session2'}),
 
-    'session2'   : genrateHtmlSessionDetails({sid:2, next: 'session2_1', previous: 'session1_16', data:{template: 'sessions/2.html'}}),
+    'session2'   : genrateSummarySessionDetails({
+        sid:2, 
+        previous: 'session1_16',
+        session: 'Statements',
+        subSessions: [
+          { url: '#side/session2_1', title: 'Expression Statements' },
+          { url: '#side/session2_2', title: 'Compound and Empty Statements' },
+          { url: '#side/session2_3', title: 'Declaration Statements' },
+          { url: '#side/session2_4', title: 'Conditionals' },
+          { url: '#side/session2_5', title: 'Jumps' },
+          { url: '#side/session2_6', title: 'Miscellaneous Statements' },
+        ]
+    }),
+    'session2_1' : genrateHtmlSessionDetails   ({sid:2, ssid: '1', previous: 'session2'}),
+    'session2_2' : genrateHtmlSessionDetails   ({sid:2, ssid: '2'}),
+    'session2_3' : genrateHtmlSessionDetails   ({sid:2, ssid: '3'}),
+    'session2_4' : genrateJsBinSessionDetails  ({sid:2, ssid: '4', url: '//jsbin.com/ashishJSVariables/2/embed?js'}),
 });
