82336a70e1b5d194dcf29547b68dc717c19924cf
OpenShift guest <ashish.kvarma@gmail.com>
2014-03-25 01:06:01 -0400

    3_14

--- app/js/views/IndexView.js
+++ app/js/views/IndexView.js
@@ -23,23 +23,22 @@ define(['jquery', 'underscore', 'backbone', 'text!partials/indexView.html'], fun
                     ]},
                     { url: '#side/session3', title: 'Inbuild Objects', topics:[
                         { url: '#side/session3_1', title: 'Objects' },
-                        { url: '#side/session3_2', title: 'Arrays' },
+                        { url: '#side/session3_12', title: 'Arrays' },
                         { url: '#side/session3_3', title: 'Functions' },
                         { url: '#side/session3_4', title: 'Regex' },
                         { url: '#side/session3_5', title: 'Date' }
                     ]},
-                    { url: '#side/session4', title: 'Opps' },
-                    { url: '#side/session5', title: 'Modules & Libraries', topics:[
-                        { url: '#side/session5_1', title: 'Modules' },
-                        { url: '#side/session5_2', title: 'Libraries' }
+                    { url: '#side/session4', title: 'Modules & Libraries', topics:[
+                        { url: '#side/session4_1', title: 'Modules' },
+                        { url: '#side/session4_2', title: 'Libraries' }
                     ]},
-                    { url: '#side/session6', title: 'The Good Parts' },
-                    { url: '#side/session7', title: 'Browser', topics:[
-                        { url: '#side/session7_1', title: 'Browser Objects' },
-                        { url: '#side/session7_2', title: 'Events' },
-                        { url: '#side/session7_3', title: 'Ajax' }
+                    { url: '#side/session5', title: 'The Good Parts' },
+                    { url: '#side/session6', title: 'Browser', topics:[
+                        { url: '#side/session6_1', title: 'Browser Objects' },
+                        { url: '#side/session6_2', title: 'Events' },
+                        { url: '#side/session6_3', title: 'Ajax' }
                     ]},
-                    { url: '#side/session8', title: 'Intoduction to HTML5' },
+                    { url: '#side/session7', title: 'Intoduction to HTML5' },
                 ]
             };
 
