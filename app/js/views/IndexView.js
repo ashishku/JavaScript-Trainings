d3c9609551851e4dbdb54939072b8122af7ae36c
OpenShift guest <ashish.kvarma@gmail.com>
2014-04-01 04:42:38 -0400

    The Good Parts

--- app/js/views/IndexView.js
+++ app/js/views/IndexView.js
@@ -4,6 +4,7 @@ define(['jquery', 'underscore', 'backbone', 'text!partials/indexView.html'], fun
         render: function(){
             var slides = {
                 sessions: [
+                  [
                     { url: '#side/session1', title: 'Intoduction to JavaScript', topics:[
                         { url: '#side/session1_1', title: 'Introduction' },
                         { url: '#side/session1_2', title: 'Lexical Structure' },
@@ -19,7 +20,7 @@ define(['jquery', 'underscore', 'backbone', 'text!partials/indexView.html'], fun
                         { url: '#side/session2_5', title: 'Conditionals' },
                         { url: '#side/session2_10', title: 'Loops' },
                         { url: '#side/session2_12', title: 'Jumps' },
-                        { url: '#side/session2_15', title: 'Miscellaneous Statements' },
+                        { url: '#side/session2_15', title: 'Miscellaneous Statements' }
                     ]},
                     { url: '#side/session3', title: 'Inbuild Objects', topics:[
                       { url: '#side/session3_1', title: 'Objects' },
@@ -38,20 +39,29 @@ define(['jquery', 'underscore', 'backbone', 'text!partials/indexView.html'], fun
                       { url: '#side/session4_9',  title: "Public and Private Variables/Methods"},
                       { url: '#side/session4_10', title: "Constructor and method chaining from subclass to superclass"},
                       { url: '#side/session4_11', title: "Abstract Class & module"}
-                    ]},
+                    ]}
+                  ],
+                  [
                     { url: '#side/session5', title: 'Modules & Libraries', topics:[
                       { url: '#side/session5_1',  title: 'Revisit Module' },
                       { url: '#side/session5_2',  title: 'Loading Module' },
                       { url: '#side/session5_4',  title: 'Require.js' },
-                      { url: '#side/session5_7',  title: 'underscore.js' },
+                      { url: '#side/session5_7',  title: 'underscore.js' }
                     ]},
-                    { url: '#side/session6', title: 'The Good Parts' },
+                    { url: '#side/session6', title: 'The Good Parts', topics:[
+                      { url: '#side/session6_1',  title: 'The Author and The Book' },
+                      { url: '#side/session6_2',  title: 'Awful Parts' },
+                      { url: '#side/session6_3',  title: 'Bad Parts' },
+                      { url: '#side/session6_4',  title: 'Examples' },
+                      { url: '#side/session6_15',  title: 'JSLint' }
+                    ]},
                     { url: '#side/session7', title: 'Browser', topics:[
                         { url: '#side/session7_1', title: 'Browser Objects' },
                         { url: '#side/session7_2', title: 'Events' },
                         { url: '#side/session7_3', title: 'Ajax' }
                     ]},
-                    { url: '#side/session8', title: 'Intoduction to HTML5' },
+                    { url: '#side/session8', title: 'Intoduction to HTML5' }
+                  ]
                 ]
             };
 
